17135b772cac40793d96b4c01a0982161acea77b
Ahmet Göktürk Kurt <ahmetgokturkkurt3@gmail.com>
2023-12-26 20:01:43 +0300

    Initial Commit

---
+++ DetailScreen/DetailScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetailScreenApp.swift
+//  DetailScreen
+//
+//  Created by Ahmet Göktürk Kurt on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DetailScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
